775c1fc94a5f17125406867511fd3a3b88d58052
August Alexandersson <augustalexandersson@gmail.com>
2019-10-27 20:48:58 +0100

    ...

--- lib/ruby_tdd.rb
+++ lib/ruby_tdd.rb
@@ -4,10 +4,14 @@ require_relative './test_watch'
 require_relative './stub'
 
 def test_case(description, block)
-  if block.call
-    test_passed_message 'Passed: "' + description + '"'
-  else
-    test_failed_message 'Failed test: "' + description + '"'
+  begin
+    if block.call
+      test_passed_message 'Passed: "' + description + '"'
+    else
+      test_failed_message 'Failed test: "' + description + '"'
+    end
+  rescue => error
+    test_failed_message 'Failed test because an error was raised: ' + error
   end
 end
 
--- tests/test.rb
+++ tests/test.rb
@@ -4,4 +4,8 @@ TestWatch.new('./lib', Proc.new {
   test_case('smoke test', Proc.new {
     1 == 1
   })
+
+  test_case('equal?', Proc.new {
+    equal? 1, 1
+  })
 })
